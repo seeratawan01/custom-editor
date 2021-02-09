06d24903f6c484d4a3d276bb53d619780cf57b57
seeratawan01 <seeratsdsking@gmail.com>
2021-02-09 21:55:34 +0500

    Overlapped

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,117 +1,113 @@
 <template>
   <div>
-    <div class="counter">Count: {{character_count }} Limit: {{this.max}}</div>
+    <div class="counter">
+      Count: {{ character_count }} Limit: {{ this.max }}
+    </div>
     <div class="editor-container">
       <textarea
-          contenteditable="true"
-          class="editor"
-          name="single-box"
-          id="single-box"
-          @input="type($event)"
+        contenteditable="true"
+        class="editor"
+        name="single-box"
+        id="single-box"
+        @input="type($event)"
       ></textarea>
-      <div
-          class='editor'
-          id='clonedEditor'
-          data-placeholder='Clone...'
-      >
+      <div class="editor" id="clonedEditor" data-placeholder="Clone...">
         <span class="desc"></span>
         <span class="exceed"></span>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import * as linkify from 'linkifyjs'
-import mention from 'linkifyjs/plugins/mention'
-import hashtag from 'linkifyjs/plugins/hashtag'
+import * as linkify from "linkifyjs";
+import mention from "linkifyjs/plugins/mention";
+import hashtag from "linkifyjs/plugins/hashtag";
 // var linkifyHtml = require('linkifyjs/html')
-mention(linkify)
-hashtag(linkify)
+mention(linkify);
+hashtag(linkify);
 
-import linkifyStr from 'linkifyjs/string';
-
+import linkifyStr from "linkifyjs/string";
 
 export default {
   name: "Editor",
   props: {
     max: {
-      default: 250
-    }
+      default: 250,
+    },
   },
-  data () {
+  data() {
     return {
-      character_count: 0
-    }
+      character_count: 0,
+    };
   },
-  components: {
-
-  },
+  components: {},
   methods: {
     type(e) {
-
-      let cloneBlock = document.getElementById('clonedEditor')
+      let cloneBlock = document.getElementById("clonedEditor");
       let ch_length = e.target.value.length;
       this.character_count = ch_length;
 
       // Removing remaing exceed charater
-      if(cloneBlock.childNodes[1].innerHTML.length && ch_length <= this.max) {
-          cloneBlock.childNodes[1].innerHTML = ""
+      if (cloneBlock.childNodes[1].innerHTML.length && ch_length <= this.max) {
+        cloneBlock.childNodes[1].innerHTML = "";
       }
 
-      if(ch_length > this.max) {
-        this.replaceInnerHTML(cloneBlock, e.target.value, 1)
+      if (ch_length > this.max) {
+        this.replaceInnerHTML(cloneBlock, e.target.value, 1);
       } else {
-       this.replaceInnerHTML(cloneBlock, e.target.value, 0)
+        this.replaceInnerHTML(cloneBlock, e.target.value, 0);
       }
 
       // Auto Resize Hack
-      e.target.style.overflow = 'hidden';
+      e.target.style.overflow = "hidden";
       e.target.style.height = "auto";
-      e.target.style.height = e.target.scrollHeight + 'px';
+      e.target.style.height = e.target.scrollHeight + "px";
     },
     replaceInnerHTML(oldDiv, text, node = 0) {
       let newDiv = oldDiv.cloneNode(true);
-      
-      if(node === 0) {
+
+      if (node === 0) {
         newDiv.childNodes[0].innerHTML = this.processTextHtml(text);
-        
       } else {
-        newDiv.childNodes[0].innerHTML = this.processTextHtml(text.substring(0, this.max));
-        newDiv.childNodes[1].innerHTML = this.processTextHtml(text.substring(this.max, text.length));
+        newDiv.childNodes[0].innerHTML = this.processTextHtml(
+          text.substring(0, this.max)
+        );
+        newDiv.childNodes[1].innerHTML = this.processTextHtml(
+          text.substring(this.max, text.length)
+        );
       }
-  
+
       oldDiv.parentNode.replaceChild(newDiv, oldDiv);
     },
-    processTextHtml (text) {
+    processTextHtml(text) {
       return linkifyStr(text, {
-        className: 'custom-linkified',
+        className: "custom-linkified",
         attributes: {
-          rel: 'nofollow'
+          rel: "nofollow",
         },
         formatHref: function (href, type) {
-          if (type === 'hashtag') {
-            href = 'https://twitter.com/hashtag/' + href.substring(1)
+          if (type === "hashtag") {
+            href = "https://twitter.com/hashtag/" + href.substring(1);
           }
 
-          if (type === 'mention') {
-            href = 'https://twitter.com/' + href.substring(1)
+          if (type === "mention") {
+            href = "https://twitter.com/" + href.substring(1);
           }
-          return href
+          return href;
         },
-         nl2br: true,
-        target: '_blank',
+        nl2br: true,
+        target: "_blank",
         // format: function (value, type) {
         //   if (type === 'url' && value.length > 50) {
         //     value = value.slice(0, 50) + '…';
         //   }
         //   return value;
         // }
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style >
@@ -126,12 +122,14 @@ export default {
   background: #f9141469;
 }
 
-#title:empty:before, #editor:empty:before {
-  content:  attr(data-placeholder);
+#title:empty:before,
+#editor:empty:before {
+  content: attr(data-placeholder);
   color: gray;
 }
+
 .editor {
-   resize: none;
+  resize: none;
   width: 95%;
   max-width: 700px;
   min-height: 100px;
@@ -144,39 +142,41 @@ export default {
   text-align: left;
   word-break: break-word;
   word-wrap: break-word;
-border: 1px solid gray;
-padding: 0.3rem;
+  border: 1px solid transparent;
+  padding: 0.3rem;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
-.editor > li {
-   list-style: square;
-   margin: initial
- }
-#title {
-    width: 95%;
-    max-width: 700px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 135px;
-    font-size: 40px;
-  }
-.editor:focus, #title:focus {
-     outline: none;
-   }
-#title:empty:before, .editor:empty:before {
-      content:  attr(data-placeholder);
-      color: gray;
-    }
+
+textarea {
+  color: transparent !important;
+  background: transparent !important;
+  border: 1px solid gray !important;
+  caret-color: red;
+  z-index: 1;
+}
+
+.editor:focus {
+  outline: none;
+}
+#title:empty:before,
+.editor:empty:before {
+  content: attr(data-placeholder);
+  color: gray;
+}
+
 .codeBlock {
-       background-color: rgb(21, 34, 75);
-       margin-left: auto;
-       margin-right: auto;
-       margin-top: 0;
-       margin-bottom: 0;
-       height: auto;
-       min-height: 39px;
-       font-family: 'Courier New', Courier, monospace;
-       overflow: auto;
-       color: white;
-       padding: 8px;
-     }
+  background-color: rgb(21, 34, 75);
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 0;
+  margin-bottom: 0;
+  height: auto;
+  min-height: 39px;
+  font-family: "Courier New", Courier, monospace;
+  overflow: auto;
+  color: white;
+  padding: 8px;
+}
 </style>
